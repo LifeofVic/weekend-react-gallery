a3d4a1a61d62c9a52ee58c7f436439c40b492b2c
Victor Llapa <victorllapa@gmail.com>
2022-11-26 13:26:18 -0600

    Added images to the images folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,13 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+	{ id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+	{ id: 2, path: 'images/arnold_shortman.jpg', description: 'Arnold from the Cartoon shoe "Hey!, Arnold".', likes: 0 },
+	{ id: 3, path: 'images/catdog.jpg.jpg', description: 'Character from the 90\'s show "CatDog".', likes: 0 },
+	{ id: 4, path: 'images/dexter.jpg', description: 'Dexter from the cartoon show "Dexter Laboratory".', likes: 0 },
+	{ id: 5, path: 'images/doug.jpg', description: 'Doug from the cartoon show "Doug".', likes: 0 },
+	{ id: 6, path: 'images/homer_simpson.jpg', description: 'Homer from the tv show "The Simpsons".', likes: 0 },
+	{ id: 7, path: 'images/johnny_bravo.jpg', description: 'Jonnny from the cartoon show "Johnny Bravo".', likes: 0 },
+	{ id: 8, path: 'images/randall_recess.jpg', description: 'Randall from "Recess".', likes: 0 },
+	{ id: 9, path: 'images/spongebob.jpg', description: 'Spongebob SquarePants from Bakini Bottom.', likes: 0 },
 ];
 
 module.exports = galleryItems;
