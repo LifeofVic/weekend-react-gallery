0e0aca170486308160b803ede41e9f27ffd57fdd
Victor Llapa <victorllapa@gmail.com>
2022-11-27 17:16:38 -0600

    finally fixed the flex-wrap of gallery-list

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-	{ id: 1, path: 'images/image_1.jpeg', description: 'Photo of Me getting promoted to SGT.', likes: 0 },
+	{ id: 1, path: 'images/image_1.jpeg', description: 'Photo of Me getting promoted to SGT while deployed.', likes: 0 },
 	{ id: 2, path: 'images/image_2.jpeg', description: 'Photo of my little man, Toby.', likes: 0 },
 	{ id: 3, path: 'images/image_3.jpeg', description: 'Photo of my siblings.', likes: 0 }
 ];
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,5 +1,5 @@
 import GalleryItem from '../GalleryItem/GalleryItem';
-
+import './GalleryList.css';
 
 function GalleryList({ list, fetchList }) {
 	return (
