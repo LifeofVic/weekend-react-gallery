046826d244683d091d7358604c796c4685860f63
Victor Llapa <victorllapa@gmail.com>
2022-11-26 18:27:57 -0600

    added to gallery list to be able to reference the set function of likeGalleryList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import GalleryList from '../GalleryList/GalleryList';
-import GalleryItem from '../GalleryItem/GalleryItem';
-import Axios from 'axios';
-function App() {
 
-const fetchList = () => {
-Axios.get('/')
-	.then(response => {})
-	
-}
+const [galleryList, setGalleryList] = useState([]);
+const [galleryItem, setGalleryItem] = useState(true);
+
+function App() {
+	useEffect(() => {
+		fetchList();
+	})
+
+	const fetchList = () => {
+		axios.get('/list')
+			.then(response => {
+				setGalleryList(response.data)
+			}).catch(error => {
+				alert('error ins Getting GalleryList')
+				console.log('Error in Axios.GET in APP.JXS', error);
+			});
+	}
+
+	const likeGalleryItem = (id) => {
+		axios.put('/list/like/${id}')
+			.then(response => {
+				fetchList();
+			}).catch(error => {
+				res.sendStatus(500);
+				console.log('Error in updating, in Axios.Put in APP.jsx')
+			});
+	}
 
 
 	return (
@@ -20,6 +40,7 @@ Axios.get('/')
 			</header>
 			<p>Gallery goes here</p>
 			<img src="images/goat_small.jpg" />
+		<GalleryList 
 		</div>
 	);
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import GalleryItem from '../GalleryItem/GalleryItem';
-import list from 'server/modules/gallery.data.js';
 
-function GalleryList(item, list) {
 
-	return (
-		<div>
-			<h2> Gallery List</h2>
-			<div className="list-container">
-				<GalleryItem item={item} />
-			</div>
-		</div>
-	);
+function GalleryList(props) {
+	const galleryimage = props.galleryList.map(gallery =>
+		<GalleryItem likeGalleryItem={props.likeGalleryItem.id} />
+	)
 }
 
 export default GalleryList;
